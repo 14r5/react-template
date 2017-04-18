02926f51b4f8ffe9df80aa02c82ab63966682154
Lars Braitinger <lars.braitinger@gmail.com>
2017-04-18 14:41:46 +0200

    add bootstrap

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -2,12 +2,14 @@
  * Created by lars on 15.04.17.
  */
 import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
 
 class App extends React.Component{
     render() {
         return(
             <div>
                 <h1>Hello World!</h1>
+                <Button>Click me!</Button>
             </div>
         );
     }
